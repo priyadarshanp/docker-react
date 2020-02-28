6fe829a586f0b582ca9284cf9d28aed4836768df
priyadarshan padigelwar <noname428797@gmail.com>
2020-02-28 19:22:23 +0530

    Changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello PD, "This is Feature branch".
+         Fuck off Zensar.
         </p>
         <a
           className="App-link"
